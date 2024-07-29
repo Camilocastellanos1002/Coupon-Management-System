72582cfa185291d04baa970f992884b049b4c2b7
Juan Camilo <jccaste1002@gmail.com>
2024-07-29 12:22:30 -0500

    coupon service update

--- src/main/java/riwi/lastfilter/spring/utils/mappers/CouponMapper.java
+++ src/main/java/riwi/lastfilter/spring/utils/mappers/CouponMapper.java
@@ -4,17 +4,17 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
-import riwi.lastfilter.spring.api.dto.Request.CouponRequest;
-import riwi.lastfilter.spring.api.dto.Response.CouponResponse;
+import riwi.lastfilter.spring.api.dto.request.CouponRequest;
+import riwi.lastfilter.spring.api.dto.response.CouponResponse;
 import riwi.lastfilter.spring.domain.entities.Coupon;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface CouponMapper {
-
-    CouponResponse toGetResp (Coupon coupon);
     
-    CouponRequest toGetReq(Coupon coupon);
+    CouponResponse toGetResp (Coupon coupon);
 
     @Mapping(target = "buys", ignore = true)
-    Coupon toGetEntity(CouponRequest request);
+    Coupon toGetEntity (CouponRequest request);
+
+    
 }
